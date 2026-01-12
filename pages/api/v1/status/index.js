0a88d225b97ab823999957c1c102eee8afbb68eb
Vicente Junior <vicentejunior4522@gmail.com>
2026-01-12 19:27:01 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ mensagem: "Hello World" });
+  const updateAt = new Date().toISOString();
+
+  const dbname = process.env.POSTGRES_DB;
+
+  const dbVersionResult = await database.query("SHOW server_version");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnections = await database.query("SHOW max_connections");
+  const dbMaxConnectionsValue = dbMaxConnections.rows[0].max_connections;
+
+  const dbUsedConnections = await database.query({
+    text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1",
+    values: [dbname],
+  });
+  const dbUsedConnectionsValue = dbUsedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updateAt,
+    depedencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: dbMaxConnectionsValue,
+        opened_connections: dbUsedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,4 +1,26 @@
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  // Faz o parse dos dados que o servidor respondeu
+  const responseBody = await response.json();
+
+  // Testa se o update_at está vindo conforme o adequado
+  console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+  const updateAtParse = new Date(responseBody.updated_at).toISOString(); // Faz o parse da data que no response
+  expect(responseBody.updated_at).toEqual(updateAtParse); //Verifica se a data que veio é igual a que fez o parse
+
+  // Testa se as informações do banco estão vindo adequadamente
+  expect(responseBody.depedencies.database.version).toBeDefined();
+  const versionDb = responseBody.depedencies.database.version;
+  expect(versionDb).toEqual("16.11");
+
+  expect(parseInt(responseBody.depedencies.database.max_connections)).toEqual(
+    100,
+  );
+
+  expect(
+    parseInt(responseBody.depedencies.database.opened_connections),
+  ).toEqual(1);
 });
