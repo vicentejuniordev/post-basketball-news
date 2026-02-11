fd518b23998cc4d5fb23f854fc60cd81d730dcb3
Vicente Junior <vicentejunior4522@gmail.com>
2026-02-11 13:04:05 +0000

    fix: change `join` to `resolve` in `api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import { resolve } from "node:path";
 import database from "infra/database.js";
 async function migrations(request, response) {
   const allowMethods = ["GET", "POST"];
@@ -26,7 +26,7 @@ async function migrations(request, response) {
     const migrationsOptions = {
       dbClient: dbClient,
       dryRun: true,
-      dir: join("infra", "migrations"),
+      dir: resolve("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
